7692c044c8bed61751e729a05a319e05c0f7a9bf
Thiago MadPin <madpin@gmail.com>
2018-10-17 18:08:26 -0300

    Changing setup.py

---
+++ setup_old.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='bitoolbox',
+    version='0.5.2',
+    author='Thiago MadPin',
+    author_email='madpin@gmail.com',
+    description="A toolbox for BI purpouses",
+    packages=[
+        'bitoolbox',
+        'bitoolbox.dbtools',
+        'bitoolbox.datetools',
+        'bitoolbox.pogtools',
+        'bitoolbox.omnitools',
+        'bitoolbox.pdutils',
+        'bitoolbox.birsttools',
+        'bitoolbox.platformtools',
+        'bitoolbox.logtools',
+        'bitoolbox.tracksaletools',
+        'bitoolbox.webtools',
+        'bitoolbox.geotools',
+    ],
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    install_requires=[
+        # 'pandas',
+        # 'configparser>=3.3.0.post2',
+        # 'pandas>=0.17.0',
+        # 'PyYAML>=3.11'
+    ])
