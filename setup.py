26eb9951d0dc5bc7b88bf79299f6a7717028c4d2
Thiago MadPin <madpin@gmail.com>
2018-10-17 18:32:27 -0300

    Fixing Typo

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'bitoobox'
+NAME = 'bitoolbox'
 DESCRIPTION = 'Many tools that I need im my carrer'
 URL = 'https://github.com/madpin/bitoolbox'
 EMAIL = 'madpin@gmail.com'
